3b6d902a91663b8b8372d73733e79821f7ebe3a0
refi93 <rafael.korbas@gmail.com>
2014-11-02 19:21:27 +0100

    first commit

---
+++ src/MyAgentb.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public  class MyAgentb extends Agent{
+	
+	Random rnd = new Random();
+	
+	public MyAgentb(World world){
+		super(world);
+	}
+		
+	public int act(int plocha[][], int[] tahy, int timeLimit){
+		int result = 0;
+		/* ZACIATOK MIESTA PRE VAS KOD */
+		
+		
+		// mozne tahy bieleho hraca v stave hry "plocha"
+		// int[] tahyBieleho = world.getPossibleMoves(plocha, World.WHITE_PLAYER);
+
+		// mozne tahy cierneho hraca v stave hry "plocha"
+		// int[] tahyCierneho = world.getPossibleMoves(plocha, World.BLACK_PLAYER);
+		
+		// stav hry po zahrati tahu "tah" hracom "player" v stave "plocha"		
+		// int[][] novaPlocha = world.getResultingState(plocha, tah, player);
+		
+		result=rnd.nextInt(tahy.length);	 
+		
+		/* KONIEC MIESTA PRE VAS KOD */
+		return result;
+	}	
+}
